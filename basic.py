82142c68e44fe8d71faf537498d0a1ebfa8e9ff3
Ramsewaktomar <ramsewakt642@gmail.com>
2024-07-11 12:17:01 +0530

    Add files via upload
    python

---
+++ basic.py
@@ -0,0 +1,185 @@
+# string
+#name="rohit"
+#print(len(name))     #  len func use for find the lenth of variable
+#new= name.replace("rohit","ritik")  # replace func use for replace the name
+#print(new)
+#upper_var=name.upper()  # upper fucntion for capital the name
+#print(upper_var)
+#name2="RAM"
+#print(name2[0])         # index use for position of name 
+#low_var=name2.lower()  # lower fun for samall the name
+#print(low_var)
+# name is variable name 
+#print(name)
+#intro =""" hello my name is 
+#ram tomar"""
+#print(intro)
+#num1="ram is great"
+#num2="ram is so cute"
+#a="ram"
+#print(num1+" "+num2)
+#found= a in num1
+#print(found)
+#num3="""Paragraphs are the building blocks of papers. Many students define paragraphs 
+# in terms of length: a paragraph is a group of at least five sentences,
+# a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences
+# is what constitutes a paragraph.
+# """
+# #name1 = "Paragraphs"
+# #found = name1 in num3
+# #print(found)
+# >>>>>>>> tuple<<<<<<<<
+
+#name =('kishan','ram','hitesh','18')
+#print(name[0:3])  # position
+# multi
+#print(name*2)
+#name2="kishan"
+#found= name2 in name
+#print(found)
+# lenth_var=name.len()
+# print(lenth_var)
+#print(len(name))
+
+#>>>>>> list<<<<<<
+#lst =[1,3,4,5]
+#print(len(list))  # for lenth
+#print(list[0:3])  #for position
+#print(list[0:5])
+#list2=[2,5,7,8,9]
+#print(list+list2)   #for addition
+#lst.insert(6,8)    # for insert  the no at position
+#print(lst)
+#lst.append(9)   # for add the number
+#print(lst)
+#print(list2.insert(1,5))
+# name =" ranjiit bhaiya"
+# print(name.count("i"))   # count is use for count the no 
+#lst.pop()
+#print(lst)
+#lst.reverse()
+#print(lst)
+#lst.remove(3)
+#print(lst)
+
+#>>>>>>>>>>>>> dictionary  <<<<<<<<<<< 
+# formate ={'key':"value"}
+#dct ={'name':"ritik",'cls':"second year","roll no":21}
+#print(dct.keys())
+#print(dct.values())
+#for key in dct:
+ #   print(key)
+#for value in dct.values():
+ #   print(value)
+#for key,value in dct.items():
+ #   print(key,value)
+#num1=" my name is ram thakur"
+#for x in num1:
+#    print(x)
+#lst=[2,4,6,8,9,1,12]
+#for x in lst:
+ #   print(x)
+#tpl =('ram','kisan','harsh',23)
+#for x in tpl:
+#    print(x)
+#set={ 'ram',35.8,'kishan'}
+#print(set)
+#for x in set:
+#    print(x) 
+#for x in range(10):
+#    print(x)
+
+#>>>>>>>>> operator <<<<<<<<<<
+#x=45
+#y=2
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x%y)
+#print(x/y)
+#print(x**y)
+#print(x//y)
+
+# condition in python 
+#x=4
+# if
+# else
+# elif 
+#if(x!=5):
+ # print("true")
+#elif(x==5):
+ # print("equale")
+#else:
+ # print("false")
+
+#>>>>>>>>>>> assingment operator >>>>>>
+#x=5
+# print(x)
+# x+=3
+# print(x)
+#x-=2
+#print(x)  
+#x**=2
+#print(x)
+#x*=2
+#print(x)
+#x%=5
+#print(x)
+
+#>>>>>>>>>> ligical operator <<<<<<<<<<
+#a =5
+#b=10
+#c=15
+#if a<b and b<c:
+ #   print("true")
+#if a<b or b>c:
+ #   print("false")
+#if not c<a:
+ #   print("not") 
+#print("ram")
+#name= "ram"
+#for i in range (10):
+#    print(name)
+#n=5
+#for i in range (11):
+#    print(n*i)
+#a=5
+#b=3
+#result=5&3    # bitwise and oprator
+#print(result)
+#result=5|3      # bitwise or oprator
+#print(result)
+#a= [1,2,3,4]
+#b=a
+#c=[1,2,4]
+#print(a is b)
+#print(a is c)
+#print(a is not b)
+#print(a is not c)
+#fruit =["apple","banana","cherry"]
+#name ="apple"
+#print(name in fruit)
+#print(name not in fruit)
+#a=5
+#b=8
+#c=9
+#if a== b:
+#    print("kishan")
+#elif a<b:
+   # print("ram")
+#else:
+#    print("harsh")
+
+# >>>>>>> looping <<<<<<<<<<<
+#a=0
+#while a<10:
+ #   a+=1
+  #  print(a)
+   # if a==7:
+    #    break
+lst=[1,2,3,5,7,"ram","harsh","kishan"]
+for i in lst:
+
+
+
+
